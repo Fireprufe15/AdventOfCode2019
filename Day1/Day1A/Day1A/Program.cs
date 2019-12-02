87c88c040d385d1cc0b543747dfb4dc0839500d9
MG Potgieter <mgandr@gmail.com>
2019-12-02 13:02:09 +0200

    stuffies

---
+++ Day1/Day1A/Day1A/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Day1B {
+   class Program {
+      static void Main(string[] args) {
+         using (var fs = new FileStream("in.txt",FileMode.Open)) {
+
+            using (TextReader textReader = new StreamReader(fs)) {
+
+               var items = textReader.ReadToEnd();
+               var itemsArray = items.Split('\n');
+
+               int fuelRequirement = 0;
+               foreach (var item in itemsArray) {
+                  if (!String.IsNullOrEmpty(item)) fuelRequirement += CalcModuleFuel(double.Parse(item));
+               }
+
+               Console.WriteLine(fuelRequirement.ToString());
+               Console.ReadKey();
+            }
+
+         }
+      }
+
+      private static int CalcModuleFuel(double moduleMass) {
+
+         var fuelReq = Math.Floor(moduleMass / 3) - 2;
+         if (fuelReq <= 0) {
+            return 0;
+         }
+         fuelReq += CalcModuleFuel(fuelReq);
+         return fuelReq;
+
+      }
+   }
+}
---
+++ Day2/Day2A/Day2A/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace Day2A {
+   class Program {
+      static void Main(string[] args) {
+         using (var fs = new FileStream("in.txt", FileMode.Open)) {
+
+            using (TextReader textReader = new StreamReader(fs)) {
+
+               var codeArray = textReader.ReadToEnd().Split(',', StringSplitOptions.RemoveEmptyEntries);
+               var codeIntArray = new int[codeArray.Length];
+               for (int i = 0; i < codeArray.Length; i++) {
+                  codeIntArray[i] = int.Parse(codeArray[i]);
+               }
+
+               for (int noun = 0; noun < 100; noun++) {
+                  for (int verb = 0; verb < 100; verb++) {
+                     for (int i = 0; i < codeArray.Length; i++) {
+                        codeIntArray[i] = int.Parse(codeArray[i]);
+                     }
+                     var output = Compute(codeIntArray, verb, noun);
+                     if (output == 19690720) {
+                        Console.WriteLine((100*noun)+verb);
+                        Console.ReadKey();
+                     }
+                  }
+               }
+               
+               Console.WriteLine(codeIntArray[0]);
+               Console.ReadKey();
+            }
+         }
+      }
+
+      private static int Compute(int[] memory, int verb, int noun) {
+
+         memory[1] = noun;
+         memory[2] = verb;
+
+         for (int i = 0; i < memory.Length; i++) {
+            switch (memory[i]) {
+               case 1:
+                  memory[memory[i + 3]] = memory[memory[i + 1]] + memory[memory[i + 2]];
+                  break;
+
+               case 2:
+                  memory[memory[i + 3]] = memory[memory[i + 1]] * memory[memory[i + 2]];
+                  break;
+
+               case 99:
+                  i = memory.Length;
+                  break;
+
+               default:
+                  break;
+            }
+            i += 3;
+         }
+
+         return memory[0];
+
+      }
+   }
+}
